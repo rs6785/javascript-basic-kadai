71ef6d4293c500f9c123813bf61cd84ed821d62f
rs6785 <reo48329@gmail.com>
2025-09-14 18:44:45 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,6 @@
 const pushBtn=document.getElementById("btn");
-
+const text =document.getElementById("text");
 pushBtn.addEventListener('click',()=>{
-    const text =document.getElementById("text");
     text.textContent="ボタンをクリックしました";
 
 })
